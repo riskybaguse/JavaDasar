0f0c3c7a6035473fb90155f7ec3a3fc68afbb8d0
riskybaguse <riskybaguse474@gmail.com>
2025-03-10 17:01:56 +0700

    Membuat Class TipeDataBoolean.java

---
+++ src/TipeDataBoolean.java
@@ -0,0 +1,9 @@
+public class TipeDataBoolean {
+    public static void main(String[] args) {
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println("Ini adalah "+benar);
+        System.out.println(salah);
+    }
+}
