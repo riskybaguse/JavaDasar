c7480f4c3bbaa8d1a9136b33ef47f671facee09f
riskybaguse <riskybaguse474@gmail.com>
2025-03-12 15:00:25 +0700

    Membuat Class Variable.java

---
+++ src/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "riskybaguse";
+
+        System.out.println("Nama saya adalah "+name);
+
+        int age;
+        age = 20;
+
+        System.out.println("Umur saya adalah "+age);
+    }
+}
