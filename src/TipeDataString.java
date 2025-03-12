ce15988a56230fd54e7d65273fbdf44ddb5dcfc7
riskybaguse <riskybaguse474@gmail.com>
2025-03-12 14:52:18 +0700

    Membuat Class TipeDataString.java

---
+++ src/TipeDataString.java
@@ -0,0 +1,10 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName    = "Rizky Bagus";
+        String lastName     = "Wibowo";
+        String fullName     = firstName + " " + lastName;
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+    }
+}
